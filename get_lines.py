1d8c808ec3c7e651b9dd2bac378d3aa35c6249de
dima <dom@amfp>
2023-02-01 18:23:12 +0300

    create main_file

--- get_lines.py
+++ get_lines.py
@@ -1,14 +1,32 @@
-import requests
+import os
+from sys import argv
+import pygame
+from geocode_api import get_coordinates
+from static_maps_api import get_map
 
-server = input()
-port = input()
-a = input()
-b = input()
 
-response = requests.get(f'{server}:{port}', params={
-    'a': a,
-    'b': b,
-})
-data = response.json()
-print(*sorted(data['result']))
-print(data['check'])
+def main() -> None:
+    toponym_to_find = ' '.join(argv[1:])
+
+    if toponym_to_find == '':
+        print('No data')
+        exit()
+
+    lat, lon = get_coordinates(toponym_to_find)
+    show_map((lat, lon), (0.005, 0.005), 'map')
+
+
+def show_map(ll: tuple[float, float], spn: tuple[float, float], map_type: str) -> None:
+    map_filename = get_map(ll, spn, map_type)
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_filename), (0, 0))
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+    os.remove(map_filename)
+
+
+if __name__ == '__main__':
+    main()
