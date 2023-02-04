95d0738f7c9c06c1f773e1645b5c01c37cb01796
dima <dom@amfp>
2023-02-04 14:15:54 +0300

    move func show map to a separate file

--- get_lines.py
+++ get_lines.py
@@ -1,12 +1,10 @@
-import os
 from sys import argv
-import pygame
+from pygame_show_map import show_map
 
-from geocoder_api import (
+from geocode_api import (
     get_coordinates,
     get_coordinates_and_span,
 )
-from static_maps_api import get_map
 
 
 def main() -> None:
@@ -26,23 +24,6 @@ def main() -> None:
 
     # Добавляем исходную точку на карту.
     show_map(ll=(lat, lon), spn=(dx, dy), map_type='map', pt=(lat, lon))
-
-
-def show_map(*, ll: tuple[float, float], spn: tuple[float, float], map_type: str,
-             **kwarg: tuple[float, float]) -> None:
-    map_filename = get_map(ll=ll, spn=spn, map_type=map_type, **kwarg)
-    # Инициализируем pygame
-    pygame.init()
-    screen = pygame.display.set_mode((600, 450))
-    # Рисуем картинку, загружаемую из только что созданного файла.
-    screen.blit(pygame.image.load(map_filename), (0, 0))
-    # Переключаем экран и ждем закрытия окна.
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
-    # Удаляем за собой файл с изображением.
-    os.remove(map_filename)
 
 
 main()
